206bb4e19ec242e25e8295889fa7b4d57d4dc768
Philip Stoev <pstoev@materialize.io>
2022-09-29 11:05:11 +0200

    test case for gh15067

--- test/bounded-memory/mzcompose.py
+++ test/bounded-memory/mzcompose.py
@@ -8,23 +8,29 @@
 # by the Apache License, Version 2.0.
 
 from materialize.mzcompose import Composition
-from materialize.mzcompose.services import Materialized, Postgres, Testdrive
+from materialize.mzcompose.services import Materialized, Redpanda, Testdrive, Localstack
 
 SERVICES = [
+    Localstack(),
     Materialized(
 # memory="2Gb",
- extra_ports=["2104:2104"], allow_host_ports=True),
+# extra_ports=["2104:2104"], allow_host_ports=True
+    persist_blob_url = 's3://minio:minio123@persist/persist?endpoint=http://localstack:4566/&region=minio',
+),
     Testdrive(no_reset=True, seed=1, default_timeout="3600s"),
-    Postgres(),
+    Redpanda(),
 ]
 
 
 def workflow_default(c: Composition) -> None:
     """Use a Mz instance with 2Gb of RAM to ingest a >10G Postgres dataset"""
 
-    c.up("materialized", "postgres")
+    c.start_and_wait_for_tcp(services=["localstack", "redpanda"])
+    c.exec("localstack", "awslocal", "s3api", "create-bucket", "--bucket", "persist")
+
+
+    c.start_and_wait_for_tcp(services=["materialized"])
     c.wait_for_materialized()
-    c.wait_for_postgres()
     c.run("testdrive", "bounded-memory-before-restart.td")
 
     # Restart Mz to confirm that re-hydration is also bounded memory
